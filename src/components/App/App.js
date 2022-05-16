6ac34bdea98468e4b6c7b61ea7b23578d50dce0d
whoguy24 <whoguy24@gmail.com>
2022-05-16 13:10:09 -0500

    Created project folders.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,9 +2,12 @@ import './App.css';
 
 import { HashRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 
+import Navigation from '../Navigation/Navigation';
+
 function App() {
   return (
     <div className="App">
+      <Navigation />
     </div>
   );
 }
