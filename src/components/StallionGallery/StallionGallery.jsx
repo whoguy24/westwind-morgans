b6c6c7da2f1c711113ac272ac25d86f1dc791c25
whoguy24 <whoguy24@gmail.com>
2022-08-08 20:46:03 -0500

    Added banner image and began building the toolbar.

--- src/components/StallionGallery/StallionGallery.jsx
+++ src/components/StallionGallery/StallionGallery.jsx
@@ -5,17 +5,57 @@
 // Import Stylesheets
 import '../StallionGallery/StallionGallery.css';
 
+// Import Libraries
+import { useNavigate } from 'react-router-dom';
+
+// Import MUI Components
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+
 ///////////////////////////////////////////////////////
 ///// COMPONENT FUNCTION //////////////////////////////
 ///////////////////////////////////////////////////////
 
 function StallionGallery() {
 
+  // React Router Variables
+  const navigate = useNavigate();
+
   // Render DOM
   return (
     <>
 
-      <div id="stallion-gallery-banner">
+      <div id="stallion-gallery">
+
+        <div id="stallion-gallery-banner">
+          <img id="stallion-gallery-banner-image" src="/images/stallion_gallery_banner.png" alt="stallion_gallery"/>
+        </div>
+
+        <div id="stallion-gallery-header">
+          <Typography id="stallion-gallery-header-text">Stallions</Typography>
+        </div>
+
+        <div id="stallion-gallery-toolbar">
+
+          <div id="stallion-gallery-breadcrumbs">
+            <Breadcrumbs>
+              <Link underline="hover" onClick={()=>navigate("/home")}>
+                Westwind Morgans
+              </Link>
+
+              <Typography>
+                Stallions
+              </Typography>
+
+            </Breadcrumbs>
+          </div>
+
+          <div>
+          </div>
+
+        </div>
+
       </div>
       
     </>
