6efd6d227a63783fed6aa123e38fa06ab8b93fd0
Warren O'Brien <whoguy24@Warrens-MBP.localdomain>
2023-01-17 15:19:51 -0600

    Added datagrid component to Users component.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,8 +5,9 @@
 // Import Stylesheets
 import '../Users/Users.css';
 
-// Import Custom Components
-
+// Import MUI Components
+import Button from '@mui/material/Button';
+import { DataGrid } from '@mui/x-data-grid';
 
 ///////////////////////////////////////////////////////
 ///// COMPONENT FUNCTION //////////////////////////////
@@ -14,9 +15,46 @@ import '../Users/Users.css';
 
 function Users() {
 
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+  ];
+  
+  const rows = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+  ];
+
   return (
     <>
+      <div id ="users">
+        <div id="users-toolbar">
+          <Button id="users-add-user-button">Add User</Button>
+        </div>
 
+        <div id="users-grid">
+
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            disableSelectionOnClick={true}
+            hideFooterPagination={true}
+            hideFooterSelectedRowCount={true}
+            sx={{
+              boxShadow: 2,
+              border: 0.5,
+              borderColor: "var(--color-accent-300)",
+              borderRadius: 0,
+            }}
+          />
+
+        </div>
+
+        
+
+      </div>
     </>
   );
 
