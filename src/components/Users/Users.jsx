422390331a3714052fef6f678a64ff3805360060
Warren O'Brien <whoguy24@Warrens-MBP.localdomain>
2023-01-19 14:36:27 -0600

    Added validation to Users add user function dialog.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -19,10 +19,12 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 ///////////////////////////////////////////////////////
 ///// COMPONENT FUNCTION //////////////////////////////
@@ -48,6 +50,17 @@ function Users() {
   const [addUserPassword, setAddUserPassword] = useState("");
   const [addUserConfirmPassword, setAddUserConfirmPassword] = useState("");
 
+  const [addUserFirstNameError, setAddUserFirstNameError] = useState("");
+  const [addUserLastNameError, setAddUserLastNameError] = useState("");
+  const [addUserEmailError, setAddUserEmailError] = useState("");
+  const [addUserUsernameError, setAddUserUsernameError] = useState("");
+  const [addUserRoleError, setAddUserRoleError] = useState("");
+  const [addUserPasswordError, setAddUserPasswordError] = useState("");
+  const [addUserConfirmPasswordError, setAddUserConfirmPasswordError] = useState("");
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
   useEffect(() => {
       if(!error?.code && user?.id) {
           dispatch({ type: 'FETCH_USERS' });
@@ -62,23 +75,111 @@ function Users() {
     { field: "email", headerName: "Email", width: 300 },
     { field: "username", headerName: "Username", width: 120 },
   ];
+  
+  function handleAddUserDialogClose() {
+    setAddUserDialogActive(false)
+    setAddUserFirstName("")
+    setAddUserLastName("")
+    setAddUserEmail("")
+    setAddUserUsername("")
+    setAddUserRole("USER")
+    setAddUserPassword("")
+    setAddUserConfirmPassword("")
+    setAddUserFirstNameError("")
+    setAddUserLastNameError("")
+    setAddUserEmailError("")
+    setAddUserUsernameError("")
+    setAddUserRoleError("")
+    setAddUserPasswordError("")
+    setAddUserConfirmPasswordError("")
+  }
 
-
-  function ValidateEmail(mail) 
-  {
-   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-    {
-      return (true)
+  function validateFirstName() {
+    if ( addUserFirstName.length < 1 ) {
+      setAddUserFirstNameError("Required Field");
+    } else {
+      setAddUserFirstNameError("");
     }
-      alert("You have entered an invalid email address!")
-      return (false)
+  }
+
+  function validateLastName() {
+    if ( addUserLastName.length < 1 ) {
+      setAddUserLastNameError("Required Field");
+    } else {
+      setAddUserLastNameError("");
+    }
+  }
+
+  function validateEmail() {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (addUserEmail.length < 1 ) {
+      setAddUserEmailError("Required Field");
+    } else if ( !addUserEmail.match(validRegex)) {
+      setAddUserEmailError("Please enter a valid email address.")
+    } else {
+      setAddUserEmailError("")
+    }
+  }
+
+  function validateUsername() {
+    if ( addUserUsername.length < 1 ) {
+      setAddUserUsernameError("Required Field");
+    }
+    else {
+      setAddUserUsernameError("");
+    }
+  }
+
+  function validateRole() {
+    if ( addUserRole.length < 1 ) {
+      setAddUserRoleError("Required Field");
+    }
+    else if (addUserRole != "ADMIN" && addUserRole != "USER") {
+      setAddUserRoleError("Invalid Selection");
+    }
+    else {
+      setAddUserRoleError("");
+    }
+  }
+
+  function validatePassword() {
+    if ( addUserPassword.length < 1 ) {
+      setAddUserPasswordError("Required Field");
+    }
+    else {
+      setAddUserPasswordError("");
+    }
   }
   
+  function validateConfirmPassword() {
+    if ( addUserConfirmPassword.length < 1 ) {
+      setAddUserConfirmPasswordError("Required Field");
+    }
+    else if (addUserConfirmPassword != addUserPassword) {
+      setAddUserPasswordError("Passwords do not match.");
+      setAddUserConfirmPasswordError("Passwords do not match.");
+    }
+    else {
+      setAddUserPasswordError("");
+      setAddUserConfirmPasswordError("");
+    }
+  }
 
+  const handleShowPassordButton = () => {
+    setShowPassword(!showPassword)
+  };
 
+  const handleMouseDownShowPassword = (event) => {
+      event.preventDefault();
+  };
 
+  const handleShowConfirmPassordButton = () => {
+    setShowConfirmPassword(!showConfirmPassword)
+  };
 
-
+  const handleMouseDownShowConfirmPassword = (event) => {
+      event.preventDefault();
+  };
 
 
   return (
@@ -107,7 +208,7 @@ function Users() {
           />
         </div>
 
-        <Dialog open={addUserDialogActive} onClose={()=>setAddUserDialogActive(false)}>
+        <Dialog open={addUserDialogActive} onClose={handleAddUserDialogClose}>
           <DialogTitle id="users-dialog-title">New User Registration</DialogTitle>
             <DialogContent>
             <div id="users-add-user-dialog-form">
@@ -115,59 +216,65 @@ function Users() {
               <div className="users-dialog-textfield-group">
                 <TextField 
                   className="users-dialog-textfield" 
+                  required
                   label="First Name" 
                   variant="standard" 
+                  error={addUserFirstNameError}
+                  helperText={addUserFirstNameError} 
                   value={addUserFirstName} 
                   onChange={(event)=>setAddUserFirstName(event.target.value)} 
+                  onBlur={validateFirstName}
                 />
                 <TextField 
                   className="users-dialog-textfield" 
+                  required
                   label="Last Name" 
                   variant="standard"
+                  error={addUserLastNameError}
+                  helperText={addUserLastNameError} 
                   value={addUserLastName} 
                   onChange={(event)=>setAddUserLastName(event.target.value)} 
+                  onBlur={validateLastName}
                 />
               </div>
 
               <div className="users-dialog-textfield-group">
-                { false ?
                   <TextField 
                     className={"users-dialog-textfield"}
+                    required
                     label="Email" 
                     variant="standard" 
+                    error={addUserEmailError}
+                    helperText={addUserEmailError} 
                     value={addUserEmail} 
                     onChange={(event)=>setAddUserEmail(event.target.value)} 
+                    onBlur={validateEmail}
                   />
-                :
-                  <TextField 
-                    error
-                    color="warning"
-                    helperText="This thing is wrong."
-                    className={"users-dialog-textfield-error"}
-                    label="Email" 
-                    variant="standard" 
-                    value={addUserEmail} 
-                    onChange={(event)=>setAddUserEmail(event.target.value)} 
-                  />
-                }
               </div>
               
               <div className="users-dialog-textfield-group">
                 <TextField 
-                  className="users-dialog-textfield" 
+                  className="users-dialog-textfield"
+                  required
                   label="Username" 
                   variant="standard" 
+                  error={addUserUsernameError}
+                  helperText={addUserUsernameError} 
                   value={addUserUsername} 
                   onChange={(event)=>setAddUserUsername(event.target.value)} 
+                  onBlur={validateUsername}
                 />
 
                 <Select
                   labelId="demo-simple-select-label"
                   className="users-dialog-textfield"
                   variant="standard"
+                  error={addUserRoleError}
+                  helperText={addUserRoleError} 
                   value={addUserRole} 
                   label="Role"
                   onChange={(event)=>setAddUserRole(event.target.value)} 
+                  onBlur={validateRole}
                 >
                   <MenuItem value={"USER"}>USER</MenuItem>
                   <MenuItem value={"ADMIN"}>ADMIN</MenuItem>
@@ -178,27 +285,65 @@ function Users() {
               <div className="users-dialog-textfield-group">
                 <TextField 
                   className="users-dialog-textfield" 
+                  required
+                  type= {showPassword ? "text" : "password"}
                   label="Password" 
                   variant="standard" 
+                  error={addUserPasswordError}
+                  helperText={addUserPasswordError} 
                   value={addUserPassword} 
                   onChange={(event)=>setAddUserPassword(event.target.value)} 
+                  onBlur={validatePassword}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                          <IconButton 
+                              id="login-input-toggle-visibility-button"
+                              aria-label="toggle password visibility" 
+                              disableRipple
+                              onClick={handleShowPassordButton} 
+                              onMouseDown={handleMouseDownShowPassword}>
+                              {showPassword ? <Visibility /> : <VisibilityOff />}
+                          </IconButton>
+                      </InputAdornment>
+                    )
+                }}
                 />
               </div>
 
               <div className="users-dialog-textfield-group">
                 <TextField 
                   className="users-dialog-textfield" 
+                  required
+                  type= {showConfirmPassword ? "text" : "password"}
                   label="Confirm Password" 
                   variant="standard" 
+                  error={addUserConfirmPasswordError}
+                  helperText={addUserConfirmPasswordError} 
                   value={addUserConfirmPassword} 
                   onChange={(event)=>setAddUserConfirmPassword(event.target.value)} 
+                  onBlur={validateConfirmPassword}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                          <IconButton 
+                              id="login-input-toggle-visibility-button"
+                              aria-label="toggle password visibility" 
+                              disableRipple
+                              onClick={handleShowConfirmPassordButton} 
+                              onMouseDown={handleMouseDownShowConfirmPassword}>
+                              {showConfirmPassword ? <Visibility /> : <VisibilityOff />}
+                          </IconButton>
+                      </InputAdornment>
+                    )
+                }}
                 />
               </div>
 
             </div>
           </DialogContent>
           <DialogActions>
-            <Button className="users-dialog-button" onClick={()=>setAddUserDialogActive(false)}>Cancel</Button>
+            <Button className="users-dialog-button" onClick={handleAddUserDialogClose}>Cancel</Button>
             <Button className="users-dialog-button" onClick={()=>setAddUserDialogActive(false)}>Register</Button>
           </DialogActions>
         </Dialog>
